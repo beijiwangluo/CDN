fef80bd2275e65f030cb97803fd09d50fe5f4402
beijiwangluo <64509408+beijiwangluo@users.noreply.github.com>
2020-11-30 23:21:29 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('ul.navigationlist li').mousemove(function(){$(this).find('ul').slideDown(280);});$('ul.navigationlist li').mouseleave(function(){$(this).find('ul').slideUp(280);});});
